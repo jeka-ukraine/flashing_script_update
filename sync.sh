d979870af42e37aaae23501fb60189a789366f56
Yevhen Prokopenko <yevhen@MacBook-Yevhen.local>
2016-06-15 00:19:31 +0300

    added sshd check

---
+++ sync.sh
@@ -0,0 +1,13 @@
+#ls -l /var/lib/ | grep sshd
+#drwx------    2 root     root          4096 Aug 24  1991 sshd
+#if User and Group differ execute this:
+#chown root:root  /var/lib/sshd -R
+$SSHD_ROOT_NUMBER=$(ls -l /var/lib/ | grep sshd | grep -c sshd)
+if ! [ "$SSHD_ROOT_NUMBER" = "2" ]; then
+	chown root:root  /var/lib/sshd -R
+fi
+
+# f=5 
+# if ! [ "$f" = "7" ]; then
+# 	echo "not equal"
+# fi
