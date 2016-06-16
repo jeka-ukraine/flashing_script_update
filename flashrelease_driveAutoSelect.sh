c11045f0659803d550a15588a3f7b60c28066f3a
Prokopenko <YProkopenko@kiev.luxoft.com>
2016-06-16 17:32:21 +0300

    some fixes

--- flashrelease_driveAutoSelect.sh
+++ flashrelease_driveAutoSelect.sh
@@ -7,7 +7,7 @@ archive=$1
 vip_output='/media/sf_VM_Share/'
 
 ###added
-$DRIVE_NAME=$(lsblk | grep -o "sd\w11" | grep -P -o --colour "[a-z]{3}")
+DRIVE_NAME=$(lsblk | grep -o "sd\w11" | grep -P -o --colour "[a-z]{3}")
 
 if [[ $(basename "$archive") =~ $regex ]]; then
 	reltype=${BASH_REMATCH[1]}
@@ -31,18 +31,31 @@ echo "Copy VIP files to $vip_output"
 VAN_regex='VANS'
 if [[ $1 =~ $VAN_regex ]];
 then
-        prefix="$output_dir/Tools/Autogrator/resources/cards/Mfa2H2RH850Firmwarev${H2RH850_version}/master/lin_opt/BIN/MFA2_VANS_B3/"
+        prefix="$output_dir/Tools/Autogrator/resources/cards/Mfa2H2RH850Firmwarev${version}/master/lin_opt/BIN/MFA2_VANS_B3"
         #echo "VANs prefix"
 else
-        prefix="$output_dir/Tools/Autogrator/resources/cards/Mfa2H2RH850Firmwarev${H2RH850_version}/master/lin_opt/BIN/MFA2_C5_B3/"
+        prefix="$output_dir/Tools/Autogrator/resources/cards/Mfa2H2RH850Firmwarev${version}/master/lin_opt/BIN/MFA2_C5_B3"
         #echo "CARs prefix"
 fi
 
 #verification for 0.1.0 instead of current version
 ZERO_VERSION='0.1.0'
-if ! [ -f "$prefix/MFA2_VIP_C5_B3_fls_img.srec" "$vip_output/" ];
+#if ! [ -f "$prefix/MFA2_VIP_C5_B3_fls_img.srec" "$vip_output/" ];
+echo "debug test of VIP file exsting"
+echo "file exist"
+if [[ -f "$prefix/MFA2_VIP_C5_B3_fls_img.srec" ]]; then
+echo "file exist status: $?"
+fi
+
+echo "file NO exist"
+if [[ ! -f "$prefix/MFA2_VIP_C5_B3_fls_img.srec" ]]; then
+echo "file NO exist status: $?"
+echo "$prefix/MFA2_VIP_C5_B3_fls_img.srec"
+fi
+
+if [[ ! -f "$prefix/MFA2_VIP_C5_B3_fls_img.srec" ]];
 then
-        prefix="$output_dir/Tools/Autogrator/resources/cards/Mfa2H2RH850Firmwarev${ZERO_VERSION}/master/lin_opt/BIN/MFA2_C5_B3/"
+        prefix="$output_dir/Tools/Autogrator/resources/cards/Mfa2H2RH850Firmwarev${ZERO_VERSION}/master/lin_opt/BIN/MFA2_C5_B3"
 fi
 
 ####################
@@ -62,21 +75,21 @@ sudo ./Autogrator.sh deploy --cardName Mfa2M2Emmcv${version} --device /dev/$DRIV
 echo ""
 echo "============================"
 echo "Network setup; User and Group of 'sshd' verification:"
-mount -t ext4 /dev/'$DRIVE_NAME'8  /mnt/
+sudo mount -t ext4 /dev/'$DRIVE_NAME'8  /mnt/
 echo "auto eth0    
 iface eth0 inet dhcp" >> /mnt/etc/network/interfaces 
 
 #check user and groop of sshd
 $SSHD_ROOT_NUMBER=$(ls -l /var/lib/ | grep sshd | grep -c sshd)
 if ! [ "$SSHD_ROOT_NUMBER" = "2" ]; then
-	chown root:root /var/lib/sshd -R
+	sudo chown root:root /var/lib/sshd -R
 	echo "sshd user and group were changed to root."
 else
 	echo "sshd user and group were correct."
 fi
 
 sync
-umount /dev/'$DRIVE_NAME'8
+sudo umount /dev/'$DRIVE_NAME'8
 echo "Network setup; User and Group of 'sshd' verification are done."
 echo "============================"
 
@@ -84,7 +97,7 @@ echo "============================"
 echo ""
 echo "============================"
 echo "Adding scopes to 'default.sco'"
-mount -t ext4 /dev/'$DRIVE_NAME'9  /mnt/
+sudo mount -t ext4 /dev/'$DRIVE_NAME'9  /mnt/
 
 echo "VERSION 1.0
 
@@ -97,7 +110,7 @@ UICockpit.HAL_Tuner.* WARN
 UICockpit.HAL_RUI.* DEBUG
 UICockpit.HAL_infotainment.TimeAndDateServiceDelegate WARN" >> /mnt/etc/trace/default.sco 
 sync
-umount /dev/'$DRIVE_NAME'9
+sudo umount /dev/'$DRIVE_NAME'9
 echo "Scopes added."
 echo "============================"
 
